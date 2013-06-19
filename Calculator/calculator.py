31ff5e24b1eb9330a96ab2581a3db79352090600
Shikha <scimitar.sh@gmail.com>
2013-06-19 03:18:45 -0700

    calculator commit

---
+++ Calculator/calculator.py
@@ -0,0 +1,21 @@
+#Basic calculator project
+import math
+def cube(n):
+    return n**3
+    
+
+def squareroot(n):
+    if n>=0:
+        return n**0.5
+    else:
+        return "NAN"
+    
+def negate(n):
+    return -n
+
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return "NAN"
+    return n*factorial(n-1)
